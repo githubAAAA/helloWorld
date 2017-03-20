eb50290dae6deee4259d1b4a47764e211d611b9b
githubAAAA <selectgoodboy@163.com>
2017-03-20 18:18:34 +0800

    Update Hello.java

--- src/cn/test/Hello.java
+++ src/cn/test/Hello.java
@@ -4,7 +4,7 @@ public class Hello {
 
     public static void main(String[] args) {
         // TODO Auto-generated method stub
-System.out.println("hello World!111222");
+System.out.println("hello World!111");
     }
 
 }
